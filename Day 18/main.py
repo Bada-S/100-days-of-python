a09f0859bcff07f290ada0ae62aac1b45f62124c
BadaS <jamessopkin@gmail.com>
2021-06-26 14:09:51 -0500

    finished turtle gui

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from turtle import colormode, Turtle, Screen
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color('darkgoldenrod2')
+timmy.turtlesize(3,3,3)
+colormode(255)
+timmy.speed('fastest')
+
+"""
+#draw a square
+for i in range(4):
+    timmy.fd(100)
+    timmy.left(90)
+
+#draw dashed line
+timmy.right(90)
+for i in range(10):
+    if i%2==0:
+        timmy.fd(10)
+        timmy.penup()
+    else:
+        timmy.fd(10)
+        timmy.pendown()
+
+timmy.fd(100)
+timmy.right(270)
+timmy.bk(75)
+timmy.clear()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# draw shapes
+for i in range(3,11):
+    for j in range(i):
+        timmy.fd(150)
+        timmy.left(360/i)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+timmy.home()
+timmy.clear()
+
+
+# random walk
+
+directions = [0, 90, 180, 270]
+rgb = range(1,255)
+timmy.pensize(15)
+timmy.speed('fastest')
+timmy.shapesize(1)
+
+
+def rand_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return [r, b, g]
+
+
+for _ in range(200):
+    timmy.color(rand_color())
+    timmy.fd(30)
+    timmy.setheading(random.choice(directions))
+    
+"""
+
+#spirograph
+for i in range(90):
+    timmy.circle(100)
+    timmy.left(4)
+
+screen = Screen()
+
+screen.exitonclick()
